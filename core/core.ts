0d7c52b2510185fe09cfaed71b4823e7366647c7
ThiagoCosta <thiagoacnascimento@gmail.com>
2026-02-06 16:43:03 -0400

    feat: add route-error

--- core/core.ts
+++ core/core.ts
@@ -8,6 +8,7 @@ import Router from "./router.ts";
 import { customRequest } from "./http/custom-request.ts";
 import { customResponse } from "./http/custom-response.ts";
 import { bodyJson } from "./middleware/body-json.ts";
+import { RouteError } from "./utils/route-error.ts";
 
 export class Core {
   router: Router;
@@ -19,26 +20,45 @@ export class Core {
   }
 
   handler = async (request: IncomingMessage, response: ServerResponse) => {
-    const req = await customRequest(request);
-    const res = customResponse(response);
+    try {
+      const req = await customRequest(request);
+      const res = customResponse(response);
 
-    for (const middleware of this.router.middlweares) {
-      await middleware(req, res);
+      for (const middleware of this.router.middlweares) {
+        await middleware(req, res);
+      }
+
+      const matched = this.router.find(req.method || "", req.pathname);
+      if (!matched) {
+        throw new RouteError(404, "nao encotrada");
+      }
+
+      const { route, params } = matched;
+      req.params = params;
+
+      for (const middleware of route.middlewares) {
+        await middleware(req, res);
+      }
+
+      await route.handler(req, res);
+    } catch (error) {
+      if (error instanceof RouteError) {
+        console.error(
+          `${error.status} ${error.message} | ${request.method} ${request.url}`,
+        );
+        response.statusCode = error.status;
+        response.setHeader("content-type", "application/problem+json");
+        response.end(
+          JSON.stringify({ status: response.statusCode, title: error.message }),
+        );
+      } else {
+        response.statusCode = 500;
+        response.setHeader("content-type", "application/json");
+        response.end(
+          JSON.stringify({ status: response.statusCode, title: "Erro" }),
+        );
+      }
     }
-
-    const matched = this.router.find(req.method || "", req.pathname);
-    if (!matched) {
-      return res.status(404).end("Nao encontrado");
-    }
-
-    const { route, params } = matched;
-    req.params = params;
-
-    for (const middleware of route.middlewares) {
-      await middleware(req, res);
-    }
-
-    await route.handler(req, res);
   };
 
   init() {
