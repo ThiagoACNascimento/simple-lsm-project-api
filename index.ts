29cfdd11922efabca4ffcaa2ba7f20a245a7d07a
ThiagoCosta <thiagoacnascimento@gmail.com>
2026-02-06 18:48:19 -0400

    feat: add CorePrivider

--- index.ts
+++ index.ts
@@ -1,36 +1,11 @@
+import { ProductsApi } from "./api/products/index.ts";
 import { Core } from "./core/core.ts";
 import { logger } from "./core/middleware/logger.ts";
-import { RouteError } from "./core/utils/route-error.ts";
 
 const core = new Core();
 
 core.router.use([logger]);
 
-core.db.exec(/*SQL*/ `
-    CREATE TABLE IF NOT EXISTS "products"
-    (
-      "id" INTEGER PRIMARY KEY,
-      "name" TEXT,
-      "slug" TEXT NOT NULL UNIQUE,
-      "price" INTEGER
-    );
-
-    INSERT OR IGNORE INTO "products"
-      ("name", "slug", "price")
-    VALUES
-      ('Notebook', 'notebook', 3000);
-  `);
-
-core.router.get("/products/:slug", (request, response) => {
-  const { slug } = request.params;
-  const products = core.db
-    .query(/*sql*/ `SELECT * FROM "products" WHERE "slug" = ?;`)
-    .get(slug);
-  if (!products) {
-    throw new RouteError(404, "produto nao encontrados");
-  }
-
-  response.status(200).json(products);
-});
+new ProductsApi(core).init();
 
 core.init();
