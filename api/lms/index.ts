a9fa80fc99d2b33931b0a079dec52fc9969eb9ba
ThiagoCosta <thiagoacnascimento@gmail.com>
2026-02-07 16:44:33 -0400

    feat: create `selectLessons/LessonCompleted` query and `resetLesson` query

--- api/lms/index.ts
+++ api/lms/index.ts
@@ -1,3 +1,4 @@
+import { title } from "node:process";
 import { Api } from "../../core/utils/abstract.ts";
 import { RouteError } from "../../core/utils/route-error.ts";
 import { LmsQuery } from "./query.ts";
@@ -47,7 +48,16 @@ export class LmsApi extends Api {
         throw new RouteError(404, "Nenhum curso encontrado");
       }
 
-      response.status(200).json({ course, lessons });
+      const userId = 1;
+      let completed: {
+        lesson_id: number;
+        completed: string;
+      }[] = [];
+      if (userId) {
+        completed = this.query.selectLessonsCompleted(userId, course.id);
+      }
+
+      response.status(200).json({ course, lessons, completed });
     },
 
     postLesson: (request, response) => {
@@ -109,14 +119,40 @@ export class LmsApi extends Api {
         throw new RouteError(404, "Aula nao encontrada");
       }
 
-      const index = nav.findIndex(
+      const indexLessonNav = nav.findIndex(
         (lessonNav) => lessonNav.slug === lesson.slug,
       );
+      const indexPrev =
+        indexLessonNav === 0 ? null : nav.at(indexLessonNav - 1)?.slug;
+      const indexNext = nav.at(indexLessonNav + 1)?.slug ?? null;
 
-      const indexPrev = index === 0 ? null : nav.at(index - 1)?.slug;
-      const indexNext = nav.at(index + 1)?.slug ?? null;
+      const userId = 1;
+      let completed = "";
+      if (userId) {
+        const lessonCompleted = this.query.selectLessonCompleted(
+          userId,
+          lesson.id,
+        );
+        if (lessonCompleted) {
+          completed = lessonCompleted.completed;
+        }
+      }
 
-      response.status(200).json({ ...lesson, indexPrev, indexNext });
+      response.status(200).json({ ...lesson, indexPrev, indexNext, completed });
+    },
+
+    resetCourse: (request, response) => {
+      const userId = 1;
+      const { courseId } = request.body;
+      const writeResult = this.query.deleteLessonsCopleted(userId, courseId);
+
+      if (writeResult.changes === 0) {
+        throw new RouteError(400, "Erro no reset do curso");
+      }
+
+      response.status(200).json({
+        title: "Curso resetado.",
+      });
     },
   } satisfies Api["handlers"];
 
@@ -128,6 +164,7 @@ export class LmsApi extends Api {
     this.router.post("/lms/course", this.handlers.postCourse);
     this.router.get("/lms/courses", this.handlers.getCourses);
     this.router.get("/lms/course/:slug", this.handlers.getCourse);
+    this.router.delete("/lms/course/reset", this.handlers.resetCourse);
     this.router.post("/lms/lesson", this.handlers.postLesson);
     this.router.get(
       "/lms/lesson/:courseSlug/:lessonSlug",
